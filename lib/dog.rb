dc1ff98d6784bfcb0f8aa53400df05a7e48f9396
BEAU WITKA <bwitka@gmail.com>
2019-03-30 20:58:25 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -9,27 +9,11 @@ class Dog
   end 
   
   def breed 
-    @breed = breed 
+    @breed
   end 
   
   def breed=(breed)
-    @breed 
+    @breed = breed 
   end 
   
-end
-
-class Person
- 
-  def initialize(name)
-    @name = name
-  end
- 
-  def name
-    @name
-  end
- 
-  def name=(new_name)
-    @name = new_name
-  end
- 
 end
